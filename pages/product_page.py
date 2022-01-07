afbec2d08ae7ce6005cbff05744546f0f8fc6d41
amakovey <amakovey@yandex.ru>
2022-01-07 10:37:41 +0400

    offer7 bug catched

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,15 +11,21 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
         self.should_be_added_procuct(product_name, product_price)
 
+
+
+
     def should_be_add_to_basket_button(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
 
     def is_product_present(self):
+        print (self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text)
         return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text, self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
 
 
     def should_be_added_procuct(self, product_name, product_price):
-        #print(self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_ALERT).text)
+        print(self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_ALERT).text)
+
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_ALERT).text == product_name, "Product name is incorrect"
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_PRICE).text == product_price, "Product price is incorrect"
+        #return self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_ALERT).text, self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_PRICE).text
--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import time
+import pytest
 '''
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,12 +19,19 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 '''
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', ['?promo=offer0','?promo=offer1','?promo=offer2','?promo=offer3','?promo=offer4',
+                                    '?promo=offer5','?promo=offer6',
+                                  pytest.param('?promo=offer7', marks=pytest.mark.xfail),'?promo=offer8',
+                                  '?promo=offer9'])
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link}"
+    print (link)
     page = ProductPage(browser, link)
     page.open()
-    time.sleep(5)
+
     page.add_to_basket()
+
+
 
