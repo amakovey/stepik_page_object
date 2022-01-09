0083f4867a7111b0b6bb13fbee50beb80c3af9e0
amakovey <amakovey@yandex.ru>
2022-01-09 09:10:29 +0400

    Minor changes - reformat files

--- test_product_page.py
+++ test_product_page.py
@@ -55,11 +55,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    page = BasketPage(browser, link)
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
-    page.should_be_empty_basket()
-    page.should_be_empty_basket_message()
+    product_page = BasketPage(browser, browser.current_url)
+    product_page.should_be_empty_basket()
+    product_page.should_be_empty_basket_message()
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 class TestUserAddToBasketFromProductPage():
